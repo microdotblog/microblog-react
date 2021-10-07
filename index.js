3d31dfd7d14de3c8a9ca1e4a64e04a30876eb39a
Vincent Ritter <git@vincentritter.com>
2021-10-07 10:58:51 +0200

    Add react-native-navigation.

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-/**
- * @format
- */
+import { Navigation } from "react-native-navigation";
+import App from "./App";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+Navigation.registerComponent('com.myApp.WelcomeScreen', () => App);
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+   Navigation.setRoot({
+     root: {
+       stack: {
+         children: [
+           {
+             component: {
+               name: 'com.myApp.WelcomeScreen'
+             }
+           }
+         ]
+       }
+     }
+  });
+});
