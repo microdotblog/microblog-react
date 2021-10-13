9427f52f64f69151d0a76e486a69de7fe3195e4d
Vincent Ritter <git@vincentritter.com>
2021-10-13 10:36:29 +0200

    Add Splash screen activity.

---
+++ android/app/src/main/java/blog/micro/android/SplashActivity.java
@@ -0,0 +1,17 @@
+package blog.micro.android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Start home activity
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        // close splash activity
+        finish();
+    }
+}
+
