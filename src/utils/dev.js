a8ce800a352f09cd0051933534c57ae3dc2ff569
Vincent Ritter <git@vincentritter.com>
2021-11-10 11:51:00 +0100

    Ignore "two children with the same id" error.

--- src/utils/dev.js
+++ src/utils/dev.js
@@ -2,5 +2,6 @@ import { LogBox } from 'react-native';
 
 LogBox.ignoreLogs([
   "Error: Nothing to dismiss",
-  "Can't perform a React state update on an unmounted component"
+  "Can't perform a React state update on an unmounted component",
+  "Error: A stack can't contain two children with the same id"
 ])
