d7cc3b572d173dd8fad6c3127f86c759eef7466b
Vincent Ritter <git@vincentritter.com>
2021-11-27 19:15:32 +0100

    Add basic profile header.

--- src/components/profile/profile_header.js
+++ src/components/profile/profile_header.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import { View, Text, ActivityIndicator, Image, TouchableOpacity } from 'react-native';
 import MicroBlogApi, { API_ERROR } from './../../api/MicroBlogApi';
+import App from './../../stores/App';
 
 @observer
 export default class ProfileHeader extends React.Component{
@@ -28,6 +29,28 @@ export default class ProfileHeader extends React.Component{
     }
   }
   
+  _render_profile = () => {
+    const { profile } = this.state;
+    return(
+      <View style={{ padding: 8, backgroundColor: "#E5E7EB", width: '100%' }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Image source={{ uri: profile.author.avatar }} style={{ width: 50, height: 50, borderRadius: 50 }} />
+          <View style={{ marginLeft: 15 }}>
+            <Text style={{ fontWeight: '700', fontSize: 18 }}>{profile.author.name}</Text>
+            <Text style={{ fontWeight: '300' }}>@{profile._microblog.username}</Text>
+            {
+              profile.author.url != null ?
+              <TouchableOpacity onPress={() => App.open_url(profile.author.url)}>
+                <Text style={{ color: "#f80", fontWeight: '600' }}>{profile.author.url.replace("https://", "").replace("http://", "")}</Text>
+              </TouchableOpacity>
+              : null
+            }
+          </View>
+        </View>
+      </View>
+    )
+  }
+  
   render() {
     const { loading, profile } = this.state
     if(loading){
@@ -38,11 +61,7 @@ export default class ProfileHeader extends React.Component{
       )
     }
     if(!loading && profile != null){
-      return(
-        <View style={{ padding: 8, backgroundColor: "#E5E7EB", width: '100%' }}>
-          <Text>This is a profile view for {this.props.username}</Text>
-        </View>
-      )
+      return this._render_profile()
     }
     return(
       <View style={{ padding: 8, backgroundColor: "#E5E7EB", width: '100%', justifyContent: 'center', alignItems: 'center' }}>
