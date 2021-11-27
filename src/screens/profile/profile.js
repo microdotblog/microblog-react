7b8a165cebb778a3fb85985fe9abe8f635ac3b61
Vincent Ritter <git@vincentritter.com>
2021-11-27 21:48:20 +0100

    Tidy up.

--- src/screens/profile/profile.js
+++ src/screens/profile/profile.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import Auth from './../../stores/Auth';
 import WebViewModule from '../../components/web/webview_module'
 import LoginMessage from '../../components/info/login_message'
