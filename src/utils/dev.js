a0d93207afc7e9b60ef4831e6ef9936695bedc40
Vincent Ritter <git@vincentritter.com>
2021-11-30 19:50:31 +0100

    Ignore stack touch warning.

--- src/utils/dev.js
+++ src/utils/dev.js
@@ -3,5 +3,6 @@ import { LogBox } from 'react-native';
 LogBox.ignoreLogs([
   "Error: Nothing to dismiss",
   "Can't perform a React state update on an unmounted component",
-  "Error: A stack can't contain two children with the same id"
+  "Error: A stack can't contain two children with the same id",
+  "Cannot record touch end without"
 ])
