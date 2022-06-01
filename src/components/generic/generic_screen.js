ae4da1d80f65494134bec7756a5e77cf13d6760a
Vincent Ritter <git@vincentritter.com>
2022-06-01 23:11:27 +0200

    Add a generic screen component.

---
+++ src/components/generic/generic_screen.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { observer } from 'mobx-react';
+import { View } from 'react-native';
+import LoginMessage from '../info/login_message';
+import ImageModalModule from '../images/image_modal';
+import WebViewModule from '../web/webview_module';
+
+@observer
+export default class GenericScreenComponent extends React.Component{
+
+  render() {
+    return(
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        {
+          this.props.can_show_web_view != null && this.props.can_show_web_view ?
+            <>
+              {this.props.children}
+              <WebViewModule endpoint={this.props.endpoint} component_id={this.props.component_id} />
+            </>
+          :
+          <LoginMessage title={this.props.title} />
+        }
+        <ImageModalModule />
+      </View>
+    )
+  }
+
+}
--- src/screens/following/following.js
+++ src/screens/following/following.js
@@ -1,23 +1,19 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import { View } from 'react-native';
 import Auth from './../../stores/Auth';
-import WebViewModule from '../../components/web/webview_module'
-import LoginMessage from '../../components/info/login_message'
+import GenericScreenComponent from '../../components/generic/generic_screen'
 
 @observer
 export default class FollowingScreen extends React.Component{
   
   render() {
-    return(
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-				{
-					Auth.is_logged_in() && !Auth.is_selecting_user ?
-          <WebViewModule endpoint={`hybrid/following/${this.props.username}`} component_id={this.props.componentId} />
-          :
-          <LoginMessage title="Users following" />
-        }
-      </View>
+    return (
+      <GenericScreenComponent
+        can_show_web_view={Auth.is_logged_in() && !Auth.is_selecting_user}
+        endpoint={`hybrid/following/${this.props.username}`}
+        component_id={this.props.componentId}
+        title="Users following"
+      />
     )
   }
   
