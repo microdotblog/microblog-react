690a82bd267a05e1ce554b1cb8464beef6a17813
Vincent Ritter <git@vincentritter.com>
2022-06-30 13:53:19 +0200

    Theme styles for reply toolbar.

--- src/components/keyboard/reply_toolbar.js
+++ src/components/keyboard/reply_toolbar.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
 import { View, Text, TouchableOpacity, Platform } from 'react-native';
-import Auth from '../../stores/Auth';
 import Reply from '../../stores/Reply';
+import App from '../../stores/App';
 
 @observer
 export default class ReplyToolbar extends React.Component{
@@ -12,7 +12,7 @@ export default class ReplyToolbar extends React.Component{
       <View
 				style={{
 					width: '100%',
-					backgroundColor: '#E5E7EB',
+					backgroundColor: App.theme_section_background_color(),
 					...Platform.select({
 						android: {
 							position: 'absolute',
@@ -28,13 +28,13 @@ export default class ReplyToolbar extends React.Component{
 				}}
 			>
 				<TouchableOpacity style={{minWidth: 35}} onPress={() => Reply.handle_text_action("**")}>
-					<Text style={{ fontSize: 20, fontWeight: '700', textAlign: 'center', padding: 2 }}>{"**"}</Text>
+					<Text style={{ fontSize: 20, fontWeight: '700', textAlign: 'center', padding: 2, color: App.theme_text_color() }}>{"**"}</Text>
 				</TouchableOpacity>
 				<TouchableOpacity style={{minWidth: 35}} onPress={() => Reply.handle_text_action("_")}>
-					<Text style={{ fontSize: 20, fontWeight: '800', textAlign: 'center', padding: 2 }}>{"_"}</Text>
+					<Text style={{ fontSize: 20, fontWeight: '800', textAlign: 'center', padding: 2, color: App.theme_text_color() }}>{"_"}</Text>
 				</TouchableOpacity>
 				<TouchableOpacity style={{minWidth: 35}} onPress={() => Reply.handle_text_action("[]")}>
-					<Text style={{ fontSize: 20, fontWeight: '600', textAlign: 'center', padding: 2 }}>{"[ ]"}</Text>
+					<Text style={{ fontSize: 20, fontWeight: '600', textAlign: 'center', padding: 2, color: App.theme_text_color() }}>{"[ ]"}</Text>
 				</TouchableOpacity>
 				<View
 					style={{
