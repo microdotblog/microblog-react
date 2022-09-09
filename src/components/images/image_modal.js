88c266a2dfc6030496b7bc4dae6de8f8babc08d0
Vincent Ritter <git@vincentritter.com>
2022-09-09 13:47:53 +0200

    Add custom image modal header component on Android.

--- src/components/images/image_modal.js
+++ src/components/images/image_modal.js
@@ -2,8 +2,9 @@ import * as React from 'react';
 import { observer } from 'mobx-react';
 import ImageView from "react-native-image-viewing";
 import App from "../../stores/App"
-import { Platform, SafeAreaView, TouchableOpacity } from 'react-native'
+import { Platform, SafeAreaView, TouchableOpacity, Image } from 'react-native'
 import { SFSymbol } from "react-native-sfsymbols";
+import ArrowBackIcon from './../../assets/icons/arrow_back.png';
 
 @observer
 export default class ImageModalModule extends React.Component{
@@ -12,16 +13,26 @@ export default class ImageModalModule extends React.Component{
 		return (
 			<SafeAreaView style={{ position: 'absolute', left: 15, top: 15 }}>
 				<TouchableOpacity onPress={App.reset_image_modal}>
-					<SFSymbol
-						name="xmark"
-						weight="semibold"
-						scale="large"
-						color="white"
-						size={16}
-						resizeMode="center"
-						multicolor={false}
-						style={{ width: 32, height: 32 }}
-					/>
+					{
+						Platform.OS === 'ios' ?
+							<SFSymbol
+								name="xmark"
+								weight="semibold"
+								scale="large"
+								color="white"
+								size={16}
+								resizeMode="center"
+								multicolor={false}
+								style={{ width: 32, height: 32 }}
+							/>
+						:
+						<Image
+							source={ArrowBackIcon}
+							resizeMode="center"
+							style={{ width: 32, height: 32, tintColor: 'white' }}
+						/>
+					}
+					
 				</TouchableOpacity>
 			</SafeAreaView>
 		)
@@ -35,7 +46,7 @@ export default class ImageModalModule extends React.Component{
 					visible={App.image_modal_is_open}
 					onRequestClose={App.reset_image_modal}
 					swipeToCloseEnabled={false}
-					HeaderComponent={Platform.OS === 'ios' ? () => this.close_button() : null}
+					HeaderComponent={() => this.close_button()}
 				/>
 			)
 		}
