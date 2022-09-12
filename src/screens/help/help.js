5cab1f47b221ea413ebb176b2c6ec8741c187fa2
Manton Reece <manton@me.com>
2022-09-12 10:20:33 -0500

    Added the app build number. For a normal release, this isn’t needed, but because we’re going to have a potentially long 3.0 beta, I think this will help.

--- src/screens/help/help.js
+++ src/screens/help/help.js
@@ -3,7 +3,7 @@ import { observer } from 'mobx-react';
 import { View, Text, TouchableOpacity, ScrollView, Image, Platform } from 'react-native';
 import { Navigation } from 'react-native-navigation';
 import App from '../../stores/App'
-import { getVersion } from 'react-native-device-info';
+import { getVersion, getBuildNumber } from 'react-native-device-info';
 
 @observer
 export default class HelpScreen extends React.Component{
@@ -100,7 +100,7 @@ export default class HelpScreen extends React.Component{
 					</View>
 				</View>
 				<View style={{ marginTop: 15, paddingTop: 15, borderColor: App.theme_border_color(), borderTopWidth: 1 }}>
-					<Text style={{color: App.theme_text_color()}}>Version { getVersion() }</Text>
+					<Text style={{color: App.theme_text_color()}}>Version { getVersion() } (build { getBuildNumber() })</Text>
 				</View>
       </ScrollView>
     )
