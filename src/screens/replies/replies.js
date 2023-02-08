6981131c7492f5299fc5091f1062b20da5ff6578
Vincent Ritter <git@vincentritter.com>
2023-02-08 01:39:39 +0100

    Remove edit button.

--- src/screens/replies/replies.js
+++ src/screens/replies/replies.js
@@ -4,7 +4,6 @@ import { View, Text, ScrollView, RefreshControl, TouchableOpacity } from 'react-
 import Auth from './../../stores/Auth';
 import LoginMessage from '../../components/info/login_message'
 import App from '../../stores/App'
-import { SvgXml } from 'react-native-svg';
 import { Navigation } from 'react-native-navigation';
 import Replies from '../../stores/Replies';
 
@@ -69,31 +68,9 @@ export default class RepliesScreen extends React.Component{
                   <TouchableOpacity onPress={() => App.handle_url_from_webview(reply.url)}>
                     <Text style={{color: "gray", fontSize: 12}}>{reply.relative_date()}</Text>
                   </TouchableOpacity>
-                  <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                    {
-                      reply.can_edit() &&
-                      <TouchableOpacity style={{marginRight: 15, flexDirection: 'row', alignItems: 'center'}} onPress={reply.trigger_edit}>
-                        <SvgXml
-                          xml={`
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" strokeWidth=".85" fill="currentColor" viewBox="0 0 16 16">
-                              <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-                            </svg>
-                          `}
-                          width={14}
-                          height={14}
-                          style={{marginRight: 4}}
-                          stroke={"rgb(115,115,115)"}
-                          fill={"transparent"}
-                        />
-                        <Text style={{color: "rgb(115,115,115)", fontSize: 15}}>
-                          Edit
-                        </Text>
-                      </TouchableOpacity>
-                    }
-                    <TouchableOpacity onPress={reply.trigger_delete} style={{flexDirection: 'row', alignItems: 'center'}}>
-                      <Text style={{color: "rgb(239,68,68)", fontSize: 15}}>Delete...</Text>
-                    </TouchableOpacity>
-                  </View>
+                 <TouchableOpacity onPress={reply.trigger_delete} style={{flexDirection: 'row', alignItems: 'center'}}>
+                   <Text style={{color: "rgb(239,68,68)", fontSize: 15}}>Delete...</Text>
+                 </TouchableOpacity>
                 </View>
               </TouchableOpacity>
             )
