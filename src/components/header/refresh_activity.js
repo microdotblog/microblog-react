1fa7c01fb0b6d8d9cae4f43f878bb2f540b04d91
Vincent Ritter <git@vincentritter.com>
2023-02-11 20:12:00 +0100

    Show correct reload indication depending on prop type.

--- src/components/header/refresh_activity.js
+++ src/components/header/refresh_activity.js
@@ -2,14 +2,16 @@ import * as React from 'react';
 import { observer } from 'mobx-react';
 import { ActivityIndicator } from 'react-native';
 import Replies from './../../stores/Replies';
+import Auth from './../../stores/Auth';
 
 @observer
 export default class RefreshActivity extends React.Component{
   
   render() {
-   return(
-    <ActivityIndicator color="#f80" animating={Replies.is_loading}  />
-   )
+    const is_loading = this.props.type === "posts" ? Auth.selected_user.posting.selected_service.is_loading_posts : Replies.is_loading
+    return(
+      <ActivityIndicator color="#f80" animating={is_loading}  />
+    )
   }
   
 }
