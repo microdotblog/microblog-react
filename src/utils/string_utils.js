950ee7e1c29255ac56f0d601e727f4000fd3c8c2
Vincent Ritter <git@vincentritter.com>
2023-02-11 20:50:31 +0100

    Slightly refactor InsertTextStyle.

--- src/utils/string_utils.js
+++ src/utils/string_utils.js
@@ -1,21 +1,24 @@
 String.prototype.InsertTextStyle = function (value_to_insert, selection, is_link = false, url = null) {
   const start = selection.start
   const end = selection.end + 1
-  const selection_count = selection.end - selection.start
-  if (selection.start === selection.end) {
-    return this.slice(0, start) + value_to_insert + this.slice(end - 1)
+  let result = ''
+  
+  if (start === end) {
+    result = `${this.slice(0, start)}${value_to_insert}${this.slice(end - 1)}`
   }
   else {
     const beginning_text = this.slice(0, start)
-    const selection = this.slice(start, (start + selection_count))
-    const remaining_text = this.slice(start + Math.abs((end - 1) - start))
+    const selected_text = this.slice(start, end - 1)
+    const remaining_text = this.slice(end - 1)
+  
     if (is_link) {
-      const link_text = `[${selection}](${url !== null ? url : ""})`
-      return beginning_text + link_text + remaining_text
+      const link_text = `[${selected_text}](${url || ''})`
+      result = `${beginning_text}${link_text}${remaining_text}`
     }
     else {
-      return beginning_text + value_to_insert + selection + value_to_insert + remaining_text
+      result = `${beginning_text}${value_to_insert}${selected_text}${value_to_insert}${remaining_text}`
     }
-    
   }
+  
+  return result;
 }
