8915fea080eae1ef54ac5ad96979f0d23691f424
Vincent Ritter <git@vincentritter.com>
2023-02-12 01:54:32 +0100

    Add better styling.

--- src/components/sheets/posts_destination.js
+++ src/components/sheets/posts_destination.js
@@ -19,7 +19,7 @@ export default class PostsDestinationMenu extends React.Component{
         useBottomSafeAreaPadding={true}
         gestureEnabled={true}
         containerStyle={{
-          backgroundColor: App.theme_background_color_secondary()
+          backgroundColor: App.theme_background_color()
         }}
       >
         <View
@@ -28,36 +28,42 @@ export default class PostsDestinationMenu extends React.Component{
             alignItems: 'center',
             justifyContent: 'center',
             borderRadius: 16,
-            marginBottom: 25
+            marginBottom: 25,
+            flexDirection: "column"
           }}
         >
           <Text style={{ fontWeight: '800', marginBottom: 25, color: App.theme_text_color() }}>Select a blog</Text>
-          <View style={{ backgroundColor: App.theme_button_background_color(), padding: 8, borderRadius: 8, marginTop: 8 }}>
-          {
-            config.destination.map((destination) => {
-              const is_selected_blog = config.posts_destination() === destination
-              return(
-                <TouchableOpacity
-                  key={destination.uid}
-                  onPress={() => selected_service.set_active_posts_dettination(destination)}
-                  style={{
-                    padding: 8,
-                    marginBottom: 5,
-                  }}
-                >
-                  <Text style={ is_selected_blog ? { fontWeight: '600', color: App.theme_button_text_color() } : { color: App.theme_button_text_color() }}>
-                    {destination.name}
+          <View style={{ paddingHorizontal: 12, backgroundColor: App.theme_settings_group_background_color(), borderRadius: 8, flexDirection: "column"}}>
+            {
+              config.destination.map((destination, index, array) => {
+                const is_last = index === array.length - 1;
+                const is_selected_blog = config.posts_destination() === destination
+                return(
+                  <TouchableOpacity
+                    key={destination.uid}
+                    onPress={() => selected_service.set_active_posts_dettination(destination)}
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      paddingVertical: 12,
+                      borderBottomWidth: is_last ? 0 : 1,
+                      borderColor: App.theme_border_color(),
+                      minWidth: 220 // Maybe don't do this?
+                    }}
+                  >
+                    <Text style={ is_selected_blog ? { fontWeight: '700', color: App.theme_button_text_color() } : { color: App.theme_button_text_color() }}>
+                      {destination.name}
+                    </Text>
                     {
                       is_selected_blog ?
                         <Image source={CheckmarkIcon} style={{ width: 15, height: 15, tintColor: App.theme_button_text_color() }} />
                       : null
                     }
-                  </Text>
-                </TouchableOpacity>
-              )
+                  </TouchableOpacity>
+                )
+              })
             }
-            )
-          }
           </View>
         </View>
       </ActionSheet>
