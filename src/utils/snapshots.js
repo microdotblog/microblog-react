6c467b8ecf5513497a85dadf64e8a32ef92b7e51
Vincent Ritter <git@vincentritter.com>
2023-03-22 22:47:02 +0100

    Debounce Auth snapshots.

--- src/utils/snapshots.js
+++ src/utils/snapshots.js
@@ -6,6 +6,21 @@ import Tokens from './../stores/Tokens';
 import Auth from './../stores/Auth';
 import Settings from './../stores/Settings';
 
+function debounce(func, wait) {
+	let timeout
+	return function (...args) {
+		const context = this
+		clearTimeout(timeout)
+		timeout = setTimeout(() => func.apply(context, args), wait)
+	}
+}
+
+const debounce_ms = 1500
+
 onSnapshot(Tokens, snapshot => { SFInfo.setItem('Tokens', JSON.stringify(snapshot), {}), console.log("SNAPSHOT:::TOKENS") });
-onSnapshot(Auth, snapshot => { AsyncStorage.setItem('Auth', JSON.stringify(snapshot)), console.log("SNAPSHOT:::AUTH") });
+onSnapshot(Auth, debounce(
+	snapshot => {
+		AsyncStorage.setItem('Auth', JSON.stringify(snapshot));
+		console.log("SNAPSHOT:::AUTH")
+	}, debounce_ms ))
 onSnapshot(Settings, snapshot => { AsyncStorage.setItem('Settings', JSON.stringify(snapshot)), console.log("SNAPSHOT:::SETTINGS") });
