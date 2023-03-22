fae300ad24d5611f54a2601527e21d8a96af3e0f
Vincent Ritter <git@vincentritter.com>
2023-03-22 01:15:52 +0100

    Check if upload is video when copying HTML.

--- src/stores/models/posting/Upload.js
+++ src/stores/models/posting/Upload.js
@@ -9,20 +9,23 @@ export default Post = types.model('Upload', {
 	.actions(self => ({
 
 		copy_html_to_clipboard() {
-			const html = `<img src="${ self.url }" />`
+			let html = `<img src="${ self.url }" />`
+			if (self.is_video()) {
+				html = `<video controls src="${ self.url }"></video>`
+			}
 			Clipboard.setString(html)
-			Toast.showWithGravity("Image HTML copied", Toast.SHORT, Toast.CENTER)
+			Toast.showWithGravity("HTML copied", Toast.SHORT, Toast.CENTER)
 		},
 
 		copy_link_to_clipboard() {
 			Clipboard.setString(self.url)
-			Toast.showWithGravity("Image URL copied", Toast.SHORT, Toast.CENTER)
+			Toast.showWithGravity("URL copied", Toast.SHORT, Toast.CENTER)
 		},
 
 		copy_markdown_to_clipboard() {
-			const markdown = `![${ self.url }](${ self.url })`
+			let markdown = `![${ self.url }](${ self.url })`
 			Clipboard.setString(markdown)
-			Toast.showWithGravity("Image Markdown copied", Toast.SHORT, Toast.CENTER)
+			Toast.showWithGravity("Markdown copied", Toast.SHORT, Toast.CENTER)
 		}
 
 	}))
@@ -30,5 +33,8 @@ export default Post = types.model('Upload', {
 		nice_local_published_date() {
 			const date = new Date(self.published)
 			return date.toLocaleString()
+		},
+		is_video() {
+			return self.url.endsWith(".mp4") || self.url.endsWith(".mov") || self.url.endsWith(".m4v") || self.url.endsWith(".webm") || self.url.endsWith(".ogv") || self.url.endsWith(".ogg") || self.url.endsWith(".avi") || self.url.endsWith(".wmv") || self.url.endsWith(".flv") || self.url.endsWith(".swf")
 		}
 	}))
