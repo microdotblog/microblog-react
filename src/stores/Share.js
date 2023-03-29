2174fb5cc4e3e6cac55d8613c45e4f6cd25629c6
Vincent Ritter <git@vincentritter.com>
2023-03-29 11:32:04 +0200

    Fetch data.

--- src/stores/Share.js
+++ src/stores/Share.js
@@ -1,4 +1,5 @@
 import { types, flow } from 'mobx-state-tree'
+import { ShareMenuReactView } from "react-native-share-menu";
 // import Auth from './Auth'
 // import { Linking, Appearance, AppState, Platform, Dimensions } from 'react-native'
 // import { theme_options } from '../utils/navigation'
@@ -8,6 +9,11 @@ export default Share = types.model('Share', {})
 
 		hydrate: flow(function* () {
 			console.log('Share:hydrate', self)
+			ShareMenuReactView.data().then(({data}) => {
+				const { mimeType } = data[ 0 ]
+				const share_data = data[ 0 ].data
+				console.log('Share:hydrate:data', share_data, mimeType)
+			})
 		}),
 			
 	}))
