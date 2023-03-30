0f4f49e90045f23a6d7fe6e204e4f05383a2ddb3
Vincent Ritter <git@vincentritter.com>
2023-03-30 19:22:53 +0200

    Prep share sheet for AsyncStorage.

--- src/stores/Share.js
+++ src/stores/Share.js
@@ -1,27 +1,32 @@
-import { types, flow } from 'mobx-state-tree'
+import { types, flow, onSnapshot, applySnapshot } from 'mobx-state-tree'
 import { ShareMenuReactView } from "react-native-share-menu";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+// import MicroBlogApi from '../api/MicroBlogApi'
+// import MicroPubApi from '../api/MicroPubApi'
 import Tokens from './Tokens';
-// import Auth from './Auth'
-// import { Linking, Appearance, AppState, Platform, Dimensions } from 'react-native'
-// import { theme_options } from '../utils/navigation'
 
 export default Share = types.model('Share', {
 	is_loading: types.optional(types.boolean, false),
+	share_type: types.optional(types.string, "text")
 })
 	.actions(self => ({
 
 		hydrate: flow(function* () {
 			console.log('Share:hydrate', self)
 			self.trigger_loading()
+			const store = yield AsyncStorage.getItem('Share')
+			if (store) {
+				applySnapshot(self, JSON.parse(store))
+				self.is_loading = false
+				self.share_type = "text"
+			}
 			yield Tokens.hydrate()
-			ShareMenuReactView.data().then(({data}) => {
-				const { mimeType } = data[ 0 ]
-				const share_data = data[ 0 ].data
-				console.log('Share:hydrate:data', share_data, mimeType, data)
-			})
-			setTimeout(() => {
-				self.trigger_loading(false)
-			}, 2000)
+			const share_data = yield ShareMenuReactView.data()
+			console.log('Share:hydrate:data', share_data)
+			if (share_data) {
+				yield self.set_data(share_data)
+			}
+			self.trigger_loading(false)
 		}),
 
 		trigger_loading: flow(function* (is_loading = true) {
@@ -32,9 +37,12 @@ export default Share = types.model('Share', {
 		open_in_app: flow(function* () {
 			console.log('Share:open_in_app')
 			ShareMenuReactView.continueInApp()
+		}),
+
+		set_data: flow(function* (data) {
+			console.log('Share:set_data', data)
 		})
 
-			
 	}))
 	.views(self => ({
 		theme_accent_color(){
@@ -42,3 +50,5 @@ export default Share = types.model('Share', {
 		}
 	}))
 	.create()
+
+onSnapshot(Share, snapshot => { AsyncStorage.setItem('Share', JSON.stringify(snapshot)), console.log("SNAPSHOT:::SHARE") });
