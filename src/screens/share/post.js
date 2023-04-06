60fa771d622909ea5aa5f74fa214bd99a65022a6
Vincent Ritter <git@vincentritter.com>
2023-04-06 02:49:34 +0200

    Tidy up. Prep to check if string is valid URL.

--- src/screens/share/post.js
+++ src/screens/share/post.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { observer } from 'mobx-react'
-import { TextInput, Button, KeyboardAvoidingView, InputAccessoryView, View } from 'react-native'
+import { TextInput, Button, KeyboardAvoidingView, InputAccessoryView } from 'react-native'
 import Share from '../../stores/Share'
 import App from '../../stores/App'
 import AssetToolbar from '../../components/keyboard/asset_toolbar'
