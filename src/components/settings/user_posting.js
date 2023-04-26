3bd3ed779b8316d8ba730aafcf9ec111dd9d1165
Vincent Ritter <git@vincentritter.com>
2023-04-26 23:31:08 +0200

    Add simple down arrow.

--- src/components/settings/user_posting.js
+++ src/components/settings/user_posting.js
@@ -4,6 +4,7 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import FastImage from 'react-native-fast-image';
 import App from '../../stores/App'
 import Auth from '../../stores/Auth';
+import { SvgXml } from 'react-native-svg';
 
 @observer
 export default class UserPostingSettings extends React.Component{
@@ -45,8 +46,16 @@ export default class UserPostingSettings extends React.Component{
 							style={{ width: 24, height: 24, borderRadius: 50, marginRight: 8 }}
 						/>
 					</View>
-					<TouchableOpacity onPress={() => this.setState({isOpen: !this.state.isOpen})} style={{flexDirection: "row"}}>
-						<Text style={{ color: App.theme_text_color() }}>{ user.posting?.selected_service?.description() }</Text>
+					<TouchableOpacity onPress={() => this.setState({isOpen: !this.state.isOpen})} style={{flexDirection: "row", alignItems: "center"}}>
+						<Text style={{ color: App.theme_text_color() }}>{user.posting?.selected_service?.description()}</Text>
+						<SvgXml
+							xml={`
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="${App.theme_text_color()}" viewBox="0 0 24 24">
+									<path d="M7 10l5 5 5-5z"/>
+									<path d="M0 0h24v24H0z" fill="none"/>
+								</svg>
+							`} 
+						/>
 					</TouchableOpacity>
 				</View>
 				{
