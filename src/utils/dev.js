904413de684aaaafe8086d9b1d159458e9b05396
Vincent Ritter <git@vincentritter.com>
2023-08-02 16:38:41 +0200

    Tweak log ignore list.

--- src/utils/dev.js
+++ src/utils/dev.js
@@ -6,5 +6,6 @@ LogBox.ignoreLogs([
   "Error: A stack can't contain two children with the same id",
   "Cannot record touch end without",
   "Require cycle:",
-  "onAnimatedValueUpdate"
+  "onAnimatedValueUpdate",
+  "Cannot update during an existing state transition"
 ])
