bff4d78ff117ca10dc4d780a0e4769bcd548652c
Vincent Ritter <git@vincentritter.com>
2023-09-21 11:15:16 +0200

    Add new style editor buttons.

--- src/components/keyboard/post_toolbar.js
+++ src/components/keyboard/post_toolbar.js
@@ -8,6 +8,7 @@ import { postingOptionsScreen, postOptionsSettingsScreen } from '../../screens'
 import App from '../../stores/App'
 import Auth from '../../stores/Auth'
 import Share from '../../stores/Share'
+import { SvgXml } from 'react-native-svg';
 
 @observer
 export default class PostToolbar extends React.Component{
@@ -111,14 +112,34 @@ export default class PostToolbar extends React.Component{
 								</TouchableOpacity>
 							</>
 						}
-						<TouchableOpacity style={{minWidth: 35}} onPress={() => App.is_share_extension ? Share.handle_text_action("**") : posting.handle_text_action("**")}>
-							<Text style={{ fontSize: 18, fontWeight: '600', textAlign: 'center', padding: 2, color: App.theme_text_color() }}>{"**"}</Text>
+						<TouchableOpacity style={{minWidth: 30}} onPress={() => App.is_share_extension ? Share.handle_text_action("**") : posting.handle_text_action("**")}>
+							<Text style={{ fontSize: 18, fontWeight: '700', textAlign: 'center', padding: 2, color: App.theme_text_color() }}>{"b"}</Text>
 						</TouchableOpacity>
-						<TouchableOpacity style={{minWidth: 35}} onPress={() => App.is_share_extension ? Share.handle_text_action("_") : posting.handle_text_action("_")}>
-							<Text style={{ fontSize: 18, fontWeight: '600', textAlign: 'center', padding: 2, color: App.theme_text_color() }}>{"_"}</Text>
+						<TouchableOpacity style={{minWidth: 30}} onPress={() => App.is_share_extension ? Share.handle_text_action("_") : posting.handle_text_action("_")}>
+							<Text style={{ fontSize: 18, fontWeight: '600', fontStyle: "italic", textAlign: 'center', padding: 2, color: App.theme_text_color() }}>{"i"}</Text>
 						</TouchableOpacity>
-						<TouchableOpacity style={{minWidth: 35}} onPress={() => App.is_share_extension ? Share.handle_text_action("[]") : posting.handle_text_action("[]")}>
-							<Text style={{ fontSize: 18, fontWeight: '600', textAlign: 'center', padding: 2, color: App.theme_text_color() }}>{"[ ]"}</Text>
+						<TouchableOpacity style={{minWidth: 30, marginLeft: 5}} onPress={() => App.is_share_extension ? Share.handle_text_action("[]") : posting.handle_text_action("[]")}>
+							{
+								Platform.OS === 'ios' ?
+									<SFSymbol
+										name={'link'}
+										color={App.theme_text_color()}
+										style={{ height: 20, width: 20 }}
+										multicolor={true}
+									/>
+								: 		
+								<SvgXml
+									style={{
+										height: 18,
+										width: 18
+									}}
+									stroke={App.theme_button_text_color()}
+									strokeWidth={2}
+									xml='<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+										<path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+									</svg>'
+								/>				
+							}
 						</TouchableOpacity>
 						{/* <TouchableOpacity style={{minWidth: 35}} onPress={() => posting.toggle_title()}>
 							<Text style={{ fontSize: 18, fontWeight: '600', textAlign: 'center', padding: 2, color: App.theme_text_color() }}>{"T"}</Text>
