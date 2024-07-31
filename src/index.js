cdcaa9c5c0ceafc8aa8e273ebc5671948f3f3ee7
Vincent Ritter <git@vincentritter.com>
2024-08-01 00:28:44 +0200

    Remove unused navigation utils.
    This is from the lib we are replacing and no longer need.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { AppRegistry } from 'react-native';
 import MainApp from './screens/App';
 import { name as appName } from './../app.json';
 import './utils/dev';
-import './utils/navigation';
 import './utils/string_checker';
 import './utils/snapshots';
 import './utils/string_utils';
