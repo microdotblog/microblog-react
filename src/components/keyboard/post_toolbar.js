df370c67739ec12a8024e79f98171fd0c6498c9b
Vincent Ritter <git@vincentritter.com>
2024-08-01 20:22:04 +0200

    Implement correct post_service navigation.

--- src/components/keyboard/post_toolbar.js
+++ src/components/keyboard/post_toolbar.js
@@ -4,7 +4,7 @@ import { Image, Platform, ScrollView, Text, TouchableOpacity, View } from 'react
 import { SFSymbol } from 'react-native-sfsymbols'
 import PhotoLibrary from '../../assets/icons/toolbar/photo_library.png'
 import SettingsIcon from '../../assets/icons/toolbar/settings.png'
-import { postingOptionsScreen, postOptionsSettingsScreen, uploadsScreen } from '../../screens'
+import { uploadsScreen } from '../../screens'
 import App from '../../stores/App'
 import Auth from '../../stores/Auth'
 import Share from '../../stores/Share'
@@ -172,19 +172,16 @@ export default class PostToolbar extends React.Component{
 								/>				
 							}
 						</TouchableOpacity>
-						{/* <TouchableOpacity style={{minWidth: 35}} onPress={() => posting.toggle_title()}>
-							<Text style={{ fontSize: 18, fontWeight: '600', textAlign: 'center', padding: 2, color: App.theme_text_color() }}>{"T"}</Text>
-						</TouchableOpacity> */}
 						{
 							!this.props.is_post_edit && posting.selected_service?.config?.active_destination() != null && (posting.selected_service?.config?.destination?.length > 1 || (!posting.selected_service?.is_microblog && !App.is_share_extension)) ?
-							<TouchableOpacity style={{marginLeft: 8, marginRight: 8}} onPress={() => {!posting.selected_service?.is_microblog ? postOptionsSettingsScreen(App.is_share_extension ? Share.selected_user : Auth.selected_user, this.props.componentId) : App.toggle_select_destination()}}>
+							<TouchableOpacity style={{marginLeft: 8, marginRight: 8}} onPress={() => {!posting.selected_service?.is_microblog ? App.navigate_to_screen("post_service", App.is_share_extension ? Share.selected_user : Auth.selected_user) : App.toggle_select_destination()}}>
 								<Text style={{ fontSize: 16, fontWeight: '500', textAlign: 'center', color: App.theme_text_color() }}>
 									{posting.selected_service.config.active_destination().name}
 								</Text>
 							</TouchableOpacity>
 							: 
 							!this.props.is_post_edit && posting.selected_service?.config?.active_destination() == null && !posting.selected_service?.is_microblog && !App.is_share_extension ?
-							<TouchableOpacity style={{marginLeft: 8, marginRight: 8}} onPress={() => {!posting.selected_service?.is_microblog ? postOptionsSettingsScreen(App.is_share_extension ? Share.selected_user : Auth.selected_user, this.props.componentId) : App.toggle_select_destination()}}>
+							<TouchableOpacity style={{marginLeft: 8, marginRight: 8}} onPress={() => {!posting.selected_service?.is_microblog ? App.navigate_to_screen("post_service", App.is_share_extension ? Share.selected_user : Auth.selected_user) : App.toggle_select_destination()}}>
 								<Text style={{ fontSize: 16, fontWeight: '500', textAlign: 'center', color: App.theme_text_color() }}>
 									{posting.selected_service?.name}
 								</Text>
