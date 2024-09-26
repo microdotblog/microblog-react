611acff3dce57c9cfbbc7e5b22747726d07145e8
Manton Reece <manton@me.com>
2024-09-26 09:15:16 -0500

    Removed screen title for login, seems redundant.

--- src/components/info/login_message.js
+++ src/components/info/login_message.js
@@ -9,11 +9,6 @@ export default class LoginMessage extends React.Component{
   render() {
     return(
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        {
-          this.props.title ?
-            <Text style={{ color: App.theme_text_color() }}>{this.props.title}</Text>
-          : null
-        }
         <TouchableOpacity style={{ marginTop: 15 }} onPress={() => App.navigate_to_screen("Login")}>
           <Text style={{fontWeight: "700", color: App.theme_accent_color()}}>Please sign in to continue</Text>
         </TouchableOpacity>
