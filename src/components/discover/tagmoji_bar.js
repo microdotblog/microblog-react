0d512dd28c0d750889b7709d4a668a44257c6b32
Manton Reece <manton@me.com>
2024-12-05 14:28:54 -0600

    Fixed Discover search padding.

--- src/components/discover/tagmoji_bar.js
+++ src/components/discover/tagmoji_bar.js
@@ -13,83 +13,81 @@ export default class TagmojiBar extends React.Component{
   
   render() {
     if (Discover.tagmoji.length > 0) {
-      return (
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            paddingHorizontal: 15,
-            paddingVertical: 10,
-            width: '100%',
-            height: 50,
-            backgroundColor: App.theme_input_background_color(),
-          }}>
-          {
-            !Discover.search_shown ?
-            <>
-              <Text style={{color: App.theme_text_color(), maxWidth: '70%'}}>Some posts from the community.</Text>
-              <View 
-                style={{
-                  flexDirection: 'row'
-                }}
-              >
-                <TouchableOpacity
-                  style={{
-                    backgroundColor: App.theme_header_button_background_color(),
-                    borderColor: App.theme_border_color(),
-                    borderWidth: 1,
-                    padding: 4,
-                    borderRadius: 5
-                  }}
-                  onPress={() => App.open_sheet("tagmoji_menu")}
-                >
-                  <Text>{Discover.random_tagmoji}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: App.theme_header_button_background_color(),
-                    borderColor: App.theme_border_color(),
-                    borderWidth: 1,
-                    padding: 4,
-                    paddingHorizontal: 6,
-                    borderRadius: 5,
-                    marginLeft: 5,
-                    marginRight: 4
-                  }}
-                  onPress={Discover.toggle_search_bar}
-                >
-                {
-                  Platform.OS === "ios" ?
-                  <SFSymbol
-                    name={"magnifyingglass"}
-                    color={App.theme_button_text_color()}
-                    style={{ height: 18, width: 18 }}
-                  />
-                  :
-                  <Image source={SearchIcon} style={{ height: 22, width: 22, tintColor: App.theme_button_text_color() }} />
-                }
-                </TouchableOpacity>
-              </View>
-            </>
-            :
-            <SearchBar
-              placeholder="Search for people and posts"
-              onSubmitEditing={() => {Discover.trigger_search(); Keyboard.dismiss()}}
-              onChangeText={(text) => Discover.set_search_query(text)}
-              value={Discover.search_query}
-              onCancel={() => {
-                  Discover.toggle_search_bar();
+      if (Discover.search_shown) {
+        return (
+          <SearchBar
+            placeholder="Search for people and posts"
+            onSubmitEditing={() => {Discover.trigger_search(); Keyboard.dismiss()}}
+            onChangeText={(text) => Discover.set_search_query(text)}
+            value={Discover.search_query}
+            onCancel={() => {
+                Discover.toggle_search_bar();
+            }}
+          />
+        )
+      }
+      else {
+        return (
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              paddingHorizontal: 15,
+              paddingVertical: 10,
+              width: '100%',
+              height: 50,
+              backgroundColor: App.theme_input_background_color(),
+            }}>
+            <Text style={{color: App.theme_text_color(), maxWidth: '70%'}}>Some posts from the community.</Text>
+            <View 
+              style={{
+                flexDirection: 'row'
               }}
-            />        
-          }
-          
-        </View>
-      )
-    }
+            >
+              <TouchableOpacity
+                style={{
+                  backgroundColor: App.theme_header_button_background_color(),
+                  borderColor: App.theme_border_color(),
+                  borderWidth: 1,
+                  padding: 4,
+                  borderRadius: 5
+                }}
+                onPress={() => App.open_sheet("tagmoji_menu")}
+              >
+                <Text>{Discover.random_tagmoji}</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  backgroundColor: App.theme_header_button_background_color(),
+                  borderColor: App.theme_border_color(),
+                  borderWidth: 1,
+                  padding: 4,
+                  paddingHorizontal: 6,
+                  borderRadius: 5,
+                  marginLeft: 5,
+                  marginRight: 4
+                }}
+                onPress={Discover.toggle_search_bar}
+              >
+              {
+                Platform.OS === "ios" ?
+                <SFSymbol
+                  name={"magnifyingglass"}
+                  color={App.theme_button_text_color()}
+                  style={{ height: 18, width: 18 }}
+                />
+                :
+                <Image source={SearchIcon} style={{ height: 22, width: 22, tintColor: App.theme_button_text_color() }} />
+              }
+              </TouchableOpacity>
+            </View>
+          </View>
+        )
+      }
     return null
   }
-  
+ } 
 }
