8d6bb7740bfbcdf4adf398183b984c036557716b
Vincent Ritter <git@vincentritter.com>
2025-01-28 11:10:12 +0100

    Pass in profile straight to web component.

--- src/screens/profile/profile.js
+++ src/screens/profile/profile.js
@@ -16,17 +16,12 @@ export default class ProfileScreen extends React.Component{
     const { username } = this.props.route.params
     return(
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        {
-          Auth.is_logged_in() && !Auth.is_selecting_user ? 
-          <ProfileHeader username={username} />
-          : null
-        }
 				{
           Auth.is_logged_in() && !Auth.is_selecting_user && !App.should_reload_web_view() ?
-            Auth.selected_user.muting?.is_muted(username) ?
+            !Auth.selected_user.muting?.is_muted(username) ?
               <MutedMessage title={`@${username} is muted`} username={username} />
               :
-              <WebViewModule endpoint={`hybrid/posts/${username}`} component_id={this.props.componentId} />
+              <WebViewModule endpoint={`hybrid/posts/${username}`} component_id={this.props.componentId} profile={Auth.is_logged_in() && !Auth.is_selecting_user ? <ProfileHeader username={username} /> : null} />
           :
           <LoginMessage title="User posts" />
         }
