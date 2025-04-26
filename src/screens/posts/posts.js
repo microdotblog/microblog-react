97543e998c65e4dbd761cb874b70ade9892e4ff0
Manton Reece <manton@me.com>
2025-04-26 11:08:28 -0400

    Only show Drafts button if there was at least one draft in the most recent 100 posts.

--- src/screens/posts/posts.js
+++ src/screens/posts/posts.js
@@ -17,7 +17,8 @@ export default class PostsScreen extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      is_showing_drafts: false
+      is_showing_drafts_button: false,
+      is_showing_drafts_posts: false
     };
   }
   
@@ -46,30 +47,32 @@ export default class PostsScreen extends React.Component{
           </Text>
         </TouchableOpacity>
         <View style={{ flex: 1 }} />
-        <TouchableOpacity
-          style={{
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: this.state.is_showing_drafts ? App.theme_header_button_selected_color() : App.theme_header_button_background_color(),
-            borderColor: App.theme_border_color(),
-            borderWidth: 1,
-            padding: 4,
-            paddingHorizontal: 8,
-            borderRadius: 5,
-            marginLeft: 5,
-          }}
-          onPress={() => {
-            const new_is_drafts = !this.state.is_showing_drafts;
-            this.setState({ is_showing_drafts: new_is_drafts });
-            const { selected_service } = Auth.selected_user.posting;
-            const { config } = selected_service;
-            selected_service.check_for_posts_for_destination(config.posts_destination(), new_is_drafts);
-          }}
-        >
-          <Text style={{color: App.theme_button_text_color()}}>
-            Drafts
-          </Text>
-        </TouchableOpacity>
+        { this.state.is_showing_drafts_button &&
+          <TouchableOpacity
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: this.state.is_showing_drafts_posts ? App.theme_header_button_selected_color() : App.theme_header_button_background_color(),
+              borderColor: App.theme_border_color(),
+              borderWidth: 1,
+              padding: 4,
+              paddingHorizontal: 8,
+              borderRadius: 5,
+              marginLeft: 5,
+            }}
+            onPress={() => {
+              const new_is_drafts = !this.state.is_showing_drafts_posts;
+              this.setState({ is_showing_drafts_posts: new_is_drafts });
+              const { selected_service } = Auth.selected_user.posting;
+              const { config } = selected_service;
+              selected_service.check_for_posts_for_destination(config.posts_destination(), new_is_drafts);
+            }}
+          >
+            <Text style={{color: App.theme_button_text_color()}}>
+              Drafts
+            </Text>
+          </TouchableOpacity>
+        }
         <TouchableOpacity
           style={{
             justifyContent: "center",
@@ -100,17 +103,21 @@ export default class PostsScreen extends React.Component{
       <SearchBar
         placeholder="Search posts"
         onSubmitEditing={() => {Keyboard.dismiss()}}
-        onChangeText={(text) => App.set_posts_query(text, config.posts_destination(), this.state.is_showing_drafts)}
+        onChangeText={(text) => App.set_posts_query(text, config.posts_destination(), this.state.is_showing_drafts_posts)}
         value={App.post_search_query}
         onCancel={() => {
             App.toggle_post_search_is_open();
-            App.set_posts_query("", null, this.state.is_showing_drafts);
+            App.set_posts_query("", null, this.state.is_showing_drafts_posts);
         }}
       />
     )
   }
   
   _key_extractor = (item) => item.uid;
+  
+  _has_any_drafts = (posts) => {
+    return posts.some(post => post["post_status"] == "draft");
+  }
   
   render_post_item = ({ item }) => {
     return (
@@ -119,13 +126,19 @@ export default class PostsScreen extends React.Component{
   }
   
   _return_posts_list = () => {
-    const { selected_service } = Auth.selected_user.posting
-    const { config } = selected_service
+    const { selected_service } = Auth.selected_user.posting;
+    const { config } = selected_service;
+    
+    setTimeout(() => {
+      const any_drafts = this._has_any_drafts(config.posts_for_destination(this.state.is_showing_drafts_posts));    
+      this.setState({ is_showing_drafts_button: any_drafts });
+    }, 500);
+    
     return(
       <FlatList
-        centerContent={config.posts_for_destination(this.state.is_showing_drafts)?.length === 0}
-        data={config.posts_for_destination(this.state.is_showing_drafts)}
-        extraData={config.posts_for_destination(this.state.is_showing_drafts)?.length && !selected_service.is_loading_posts}
+        centerContent={config.posts_for_destination(this.state.is_showing_drafts_posts)?.length === 0}
+        data={config.posts_for_destination(this.state.is_showing_drafts_posts)}
+        extraData={config.posts_for_destination(this.state.is_showing_drafts_posts)?.length && !selected_service.is_loading_posts}
         keyExtractor={this._key_extractor}
         renderItem={this.render_post_item}
         style={{
@@ -134,13 +147,15 @@ export default class PostsScreen extends React.Component{
         }}
         ListEmptyComponent={
           <View style={{ flex: 1, padding: 12, justifyContent: 'center', alignItems: 'center' }} >
-            <Text style={{ color: App.theme_text_color() }}>No posts.</Text>
+            <Text style={{ color: App.theme_text_color() }}>
+              { this.state.is_showing_drafts_posts ? "No drafts." : "No posts." }
+            </Text>
           </View>
         }
         refreshControl={
           <RefreshControl
             refreshing={false}
-            onRefresh={() => selected_service.check_for_posts_for_destination(config.posts_destination(), this.state.is_showing_drafts)}
+            onRefresh={() => selected_service.check_for_posts_for_destination(config.posts_destination(), this.state.is_showing_drafts_posts)}
           />
         }
       />
