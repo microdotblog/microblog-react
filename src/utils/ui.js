84be810492ffb7b779418cdb6ee103974d64ad94
Manton Reece <manton@me.com>
2025-06-18 10:12:38 -0500

    We’re not building with 26 yet, so disable Liquid Glass hacks.

--- src/utils/ui.js
+++ src/utils/ui.js
@@ -2,6 +2,7 @@ import { Platform } from 'react-native';
 
 export function isLiquidGlass() {
 	return (
-		(Platform.OS == 'ios') && (parseInt(Platform.Version, 10) >= 26)
+		false
+		// (Platform.OS == 'ios') && (parseInt(Platform.Version, 10) >= 26)
 	);
 }
