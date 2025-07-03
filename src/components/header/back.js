10db78d0bee5b5190bfcc2c56d7b7f6e630ab60c
Manton Reece <manton@me.com>
2025-07-03 11:08:13 -0500

    Fixed weird padding for back button on Android.

--- src/components/header/back.js
+++ src/components/header/back.js
@@ -21,11 +21,13 @@ const BackButtonContent = observer(() => {
   else {    
     button_style = {      
       marginLeft: -20,
-      paddingHorizontal: 20,
-      paddingBottom: 10,
+      marginRight: 0,
+      marginTop: -10,
       marginBottom: -10,
+      paddingLeft: 20,
+      paddingRight: 0,
       paddingTop: 10,
-      marginTop: -10
+      paddingBottom: 10
     };
   }
   
