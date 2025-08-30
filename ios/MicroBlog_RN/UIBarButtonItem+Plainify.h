f01b22bf612472db60616b0594e425e1b691b58b
Manton Reece <manton@me.com>
2025-08-30 18:06:00 -0500

    Added override for UIBarButtonItem to hide Liquid Glass effect.

---
+++ ios/MicroBlog_RN/UIBarButtonItem+Plainify.h
@@ -0,0 +1,16 @@
+//
+//  UIBarButtonItem+Plainify.h
+//  MicroBlog_RN
+//
+//  Created by Manton Reece on 8/30/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIBarButtonItem (Plainify)
+
+@end
+
+NS_ASSUME_NONNULL_END
