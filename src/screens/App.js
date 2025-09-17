8324f70e21cd4baca516b8fa9cfee8299dee4692
Vincent Ritter <git@vincentritter.com>
2025-09-17 11:03:17 +0200

    Wrap App in KeyboardProvider.

--- src/screens/App.js
+++ src/screens/App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SheetProvider } from "react-native-actions-sheet";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { KeyboardProvider } from "react-native-keyboard-controller";
 import App from './../stores/App';
 import TabNavigator from './stacks/TabNavigator';
 import LoginScreen from './login/login';
@@ -66,143 +67,145 @@ export default class MainApp extends React.Component {
       )
     }
     return(
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <SheetProvider>
-          <NavigationContainer
-            theme={{
-            dark: App.is_dark_mode(),
-            colors: {
-              background: App.theme_background_color(),
-              text: App.theme_text_color(),
-              card: App.theme_navbar_background_color()
-            }
-          }}>
-            <Stack.Navigator initialRouteName="Tabs" screenOptions={{ headerShown: false, headerTintColor: App.theme_text_color() }}>
-              <Stack.Screen name="Tabs" component={TabNavigator} />
-              <Stack.Screen
-                name="Login"
-                component={LoginScreen}
-                options={{
-                  title: "Sign In",
-                  headerShown: true,
-                  headerBackTitle: "Back"
-                }}
-              />
-              <Stack.Group
-                screenOptions={{
-                  presentation: "modal",
-                  headerShown: true,
-                  gestureEnabled: true,
-                  headerLeft: () => <CloseModalButton />
-                }}
-              >
+      <KeyboardProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <SheetProvider>
+            <NavigationContainer
+              theme={{
+              dark: App.is_dark_mode(),
+              colors: {
+                background: App.theme_background_color(),
+                text: App.theme_text_color(),
+                card: App.theme_navbar_background_color()
+              }
+            }}>
+              <Stack.Navigator initialRouteName="Tabs" screenOptions={{ headerShown: false, headerTintColor: App.theme_text_color() }}>
+                <Stack.Screen name="Tabs" component={TabNavigator} />
                 <Stack.Screen
-                  name="Reply"
-                  component={ReplyScreen}
-                  options={({ route }) => ({
-                    headerTitle: "New Reply",
+                  name="Login"
+                  component={LoginScreen}
+                  options={{
+                    title: "Sign In",
+                    headerShown: true,
+                    headerBackTitle: "Back"
+                  }}
+                />
+                <Stack.Group
+                  screenOptions={{
+                    presentation: "modal",
+                    headerShown: true,
                     gestureEnabled: true,
-                    headerRight: () => <PostReplyButton conversation_id={route.params?.conversation_id} />
-                  })}
-                />
-                <Stack.Screen
-                  name="Help"
-                  component={HelpScreen}
-                  options={{
-                    title: "Help",
-                    gestureEnabled: true,
+                    headerLeft: () => <CloseModalButton />
                   }}
-                />
-                <Stack.Screen
-                  name="Settings"
-                  component={SettingsScreen}
-                  options={{
-                    title: "Settings"
-                  }}
-                />
-                <Stack.Screen
-                  name="PostService"
-                  component={PostOptionsSettingsScreen}
-                  options={{
-                    title: "Posting Options"
-                  }}
-                />
-                <Stack.Screen
-                  name="muting"
-                  component={MutingScreen}
-                  options={{
-                    title: "Muted Users and Keywords"
-                  }}
-                />
-                <Stack.Screen
-                  name="AddBookmark"
-                  component={AddBookmarkScreen}
-                  options={{
-                    headerTitle: "Add Bookmark"
-                  }}
-                />
-                <Stack.Screen
-                  name="AddCollection"
-                  component={AddCollectionScreen}
-                  options={{
-                    headerTitle: "Add Collection"
-                  }}
-                />
-                <Stack.Screen
-                  name="ReplyEdit"
-                  component={ReplyEditScreen}
-                  options={{
-                    headerTitle: "Update Reply",
-                    gestureEnabled: false,
-                    headerRight: () => <UpdateReplyButton />
-                  }}
-                />
-                <Stack.Screen
-                  name="PageEdit"
-                  component={PageEditScreen}
-                  options={{
-                    headerTitle: "Edit Page",
-                    gestureEnabled: false,
-                    headerLeft: () => <ClosePostClearButton />,
-                    headerRight: () => <UpdatePageButton />
-                  }}
-                />
-                <Stack.Screen
-                  name="PostEdit"
-                  component={PostEditStack}
-                  options={{
-                    headerTitle: "Edit Post",
-                    gestureEnabled: false,
-                    headerShown: false
-                  }}
-                />
-                <Stack.Screen
-                  name="Posting"
-                  component={Posting}
-                  options={{
-                    headerTitle: "New Post",
-                    gestureEnabled: false,
-                    headerShown: false
-                  }}
-                />
-                {
-                  Platform.OS === "android" &&
+                >
                   <Stack.Screen
-                    name="Share"
-                    component={ShareScreen}
+                    name="Reply"
+                    component={ReplyScreen}
+                    options={({ route }) => ({
+                      headerTitle: "New Reply",
+                      gestureEnabled: true,
+                      headerRight: () => <PostReplyButton conversation_id={route.params?.conversation_id} />
+                    })}
+                  />
+                  <Stack.Screen
+                    name="Help"
+                    component={HelpScreen}
                     options={{
-                      headerTitle: "Share",
+                      title: "Help",
+                      gestureEnabled: true,
+                    }}
+                  />
+                  <Stack.Screen
+                    name="Settings"
+                    component={SettingsScreen}
+                    options={{
+                      title: "Settings"
+                    }}
+                  />
+                  <Stack.Screen
+                    name="PostService"
+                    component={PostOptionsSettingsScreen}
+                    options={{
+                      title: "Posting Options"
+                    }}
+                  />
+                  <Stack.Screen
+                    name="muting"
+                    component={MutingScreen}
+                    options={{
+                      title: "Muted Users and Keywords"
+                    }}
+                  />
+                  <Stack.Screen
+                    name="AddBookmark"
+                    component={AddBookmarkScreen}
+                    options={{
+                      headerTitle: "Add Bookmark"
+                    }}
+                  />
+                  <Stack.Screen
+                    name="AddCollection"
+                    component={AddCollectionScreen}
+                    options={{
+                      headerTitle: "Add Collection"
+                    }}
+                  />
+                  <Stack.Screen
+                    name="ReplyEdit"
+                    component={ReplyEditScreen}
+                    options={{
+                      headerTitle: "Update Reply",
+                      gestureEnabled: false,
+                      headerRight: () => <UpdateReplyButton />
+                    }}
+                  />
+                  <Stack.Screen
+                    name="PageEdit"
+                    component={PageEditScreen}
+                    options={{
+                      headerTitle: "Edit Page",
+                      gestureEnabled: false,
+                      headerLeft: () => <ClosePostClearButton />,
+                      headerRight: () => <UpdatePageButton />
+                    }}
+                  />
+                  <Stack.Screen
+                    name="PostEdit"
+                    component={PostEditStack}
+                    options={{
+                      headerTitle: "Edit Post",
                       gestureEnabled: false,
                       headerShown: false
                     }}
                   />
-                }
-              </Stack.Group>
-            </Stack.Navigator>
-          </NavigationContainer>
-          <PublishingProgress />
-        </SheetProvider>
-      </GestureHandlerRootView>
+                  <Stack.Screen
+                    name="Posting"
+                    component={Posting}
+                    options={{
+                      headerTitle: "New Post",
+                      gestureEnabled: false,
+                      headerShown: false
+                    }}
+                  />
+                  {
+                    Platform.OS === "android" &&
+                    <Stack.Screen
+                      name="Share"
+                      component={ShareScreen}
+                      options={{
+                        headerTitle: "Share",
+                        gestureEnabled: false,
+                        headerShown: false
+                      }}
+                    />
+                  }
+                </Stack.Group>
+              </Stack.Navigator>
+            </NavigationContainer>
+            <PublishingProgress />
+          </SheetProvider>
+        </GestureHandlerRootView>
+      </KeyboardProvider>
     )
   }
   
