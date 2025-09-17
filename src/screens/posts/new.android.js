703c96c63e8db6a54fad0aac2c1b0b19bc6a6968
Vincent Ritter <git@vincentritter.com>
2025-09-17 13:01:23 +0200

    Change padding depending on presence of post assets.

--- src/screens/posts/new.android.js
+++ src/screens/posts/new.android.js
@@ -61,7 +61,7 @@ export default class PostingScreen extends React.Component{
               marginTop: 3,
               padding: 8,
               color: App.theme_text_color(),
-              paddingBottom: 50
+              paddingBottom: posting.post_assets?.length > 0 ? 80 : 50,
             }}
             editable={!posting.is_sending_post}
             multiline={true}
