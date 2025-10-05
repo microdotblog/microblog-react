7f6b6f56bdec82b08a9e95b3de71144cd9b89d24
Vincent Ritter <git@vincentritter.com>
2025-10-05 21:56:19 +0200

    Add new style keyboard avoiding toolbar to ios. Show usernames after assets toolbar.

--- src/screens/posts/new.ios.js
+++ src/screens/posts/new.ios.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import { View, TextInput, InputAccessoryView } from 'react-native';
+import { TextInput } from 'react-native';
+import { KeyboardAvoidingView, KeyboardStickyView } from "react-native-keyboard-controller";
 import Auth from '../../stores/Auth';
 import App from '../../stores/App';
 import PostToolbar from '../../components/keyboard/post_toolbar';
@@ -24,79 +25,81 @@ export default class PostingScreen extends React.Component{
   render() {
     const { posting } = Auth.selected_user
     return(
-      <View style={{ flex: 1, backgroundColor: App.theme_background_color() }}>
-        {
-          posting.should_show_title() ?
-          <TextInput
-            placeholder="Title"
-            placeholderTextColor={App.theme_placeholder_text_color()}
+      <>
+        <KeyboardAvoidingView style={{ flex: 1 }}>
+          {
+            posting.should_show_title() ?
+            <TextInput
+              placeholder="Title"
+              placeholderTextColor={App.theme_placeholder_text_color()}
+              style={{
+                fontSize: 18,
+                justifyContent: 'flex-start',
+  						  alignItems: 'flex-start',
+                padding: 8,
+                marginBottom: 4,
+                fontWeight: '700',
+                borderColor: App.theme_border_color(),
+                borderBottomWidth: .5,
+                color: App.theme_text_color()
+              }}
+              editable={!posting.is_sending_post}
+              multiline={false}
+              scrollEnabled={false}
+              returnKeyType={'default'}
+  					  keyboardType={'default'}
+  					  autoFocus={false}
+  					  autoCorrect={true}
+  					  clearButtonMode={'while-editing'}
+  					  enablesReturnKeyAutomatically={true}
+  					  underlineColorAndroid={'transparent'}
+              value={posting.post_title}
+              onChangeText={(text) => !posting.is_sending_post ? posting.set_post_title(text) : null}
+              inputAccessoryViewID={this.input_accessory_view_id}
+            />
+            : null
+          }
+          <HighlightingText
+            placeholderTextColor="lightgrey"
             style={{
+              minHeight: 300,
               fontSize: 18,
               justifyContent: 'flex-start',
-						  alignItems: 'flex-start',
+              alignItems: 'flex-start',
+              marginTop: 0,
+              paddingBottom: posting.post_text_length() > posting.max_post_length() ? 150 : 0,
+              flex: 1,
               padding: 8,
-              marginBottom: 4,
-              fontWeight: '700',
-              borderColor: App.theme_border_color(),
-              borderBottomWidth: .5,
               color: App.theme_text_color()
             }}
             editable={!posting.is_sending_post}
-            multiline={false}
-            scrollEnabled={false}
+            multiline={true}
+            scrollEnabled={true}
             returnKeyType={'default'}
-					  keyboardType={'default'}
-					  autoFocus={false}
-					  autoCorrect={true}
-					  clearButtonMode={'while-editing'}
-					  enablesReturnKeyAutomatically={true}
-					  underlineColorAndroid={'transparent'}
-            value={posting.post_title}
-            onChangeText={(text) => !posting.is_sending_post ? posting.set_post_title(text) : null}
+            keyboardType={'default'}
+            autoFocus={true}
+            autoCorrect={true}
+            clearButtonMode={'while-editing'}
+            enablesReturnKeyAutomatically={true}
+            underlineColorAndroid={'transparent'}
+            value={posting.post_text}
+            selection={posting.text_selection_flat}
+            onChangeText={({ nativeEvent: { text } }) => {
+              !posting.is_sending_post ? posting.set_post_text_from_typing(text) : null
+            }}
+            onSelectionChange={({ nativeEvent: { selection } }) => {
+              posting.set_text_selection(selection)
+            }}
             inputAccessoryViewID={this.input_accessory_view_id}
           />
-          : null
-        }
-        <HighlightingText
-          placeholderTextColor="lightgrey"
-          style={{
-            minHeight: 300,
-            fontSize: 18,
-            justifyContent: 'flex-start',
-            alignItems: 'flex-start',
-            marginTop: 0,
-            paddingBottom: posting.post_text_length() > posting.max_post_length() ? 150 : 0,
-            flex: 1,
-            padding: 8,
-            color: App.theme_text_color()
-          }}
-          editable={!posting.is_sending_post}
-          multiline={true}
-          scrollEnabled={true}
-          returnKeyType={'default'}
-          keyboardType={'default'}
-          autoFocus={true}
-          autoCorrect={true}
-          clearButtonMode={'while-editing'}
-          enablesReturnKeyAutomatically={true}
-          underlineColorAndroid={'transparent'}
-          value={posting.post_text}
-          selection={posting.text_selection_flat}
-          onChangeText={({ nativeEvent: { text } }) => {
-            !posting.is_sending_post ? posting.set_post_text_from_typing(text) : null
-          }}
-          onSelectionChange={({ nativeEvent: { selection } }) => {
-            posting.set_text_selection(selection)
-          }}
-          inputAccessoryViewID={this.input_accessory_view_id}
-        />
-        <InputAccessoryView nativeID={this.input_accessory_view_id}>
-          <UsernameToolbar componentId={this.props.componentId} object={posting} />
+          <LoadingComponent should_show={posting.is_sending_post && posting.post_text != ""} />
+        </KeyboardAvoidingView>
+        <KeyboardStickyView>
           <AssetToolbar componentId={this.props.componentId} />
+          <UsernameToolbar componentId={this.props.componentId} object={posting} />
           <PostToolbar componentId={this.props.componentId} />
-        </InputAccessoryView>
-        <LoadingComponent should_show={posting.is_sending_post && posting.post_text != ""} />
-      </View>
+        </KeyboardStickyView>
+      </>
     )
   }
   
