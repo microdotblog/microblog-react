814d17e42b0de8078327f3c5808461b428f7b0cf
Vincent Ritter <git@vincentritter.com>
2025-12-15 20:25:17 +0100

    Add Keyboard Provider for Android share extension.

--- src/screens/share/index.android.js
+++ src/screens/share/index.android.js
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react'
 import * as React from 'react'
-import { ActivityIndicator, Button, Text, View, Platform } from 'react-native'
+import { ActivityIndicator, Button, Text, View } from 'react-native'
+import { KeyboardProvider } from "react-native-keyboard-controller"
 import App from '../../stores/App'
 import Share from '../../stores/Share'
 import Auth from '../../stores/Auth';
@@ -13,59 +14,62 @@ export default class ShareScreen extends React.Component {
 
 	render() {
 		return (
-			<View style={{
-				flex: 1,
-				backgroundColor: App.theme_background_color()
-			}}>
-				{
-					Share.is_loading ?
-						<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-							<ActivityIndicator color={App.theme_accent_color()} size="large" />
-						</View>
-						:
-						Share.is_logged_in() ?
-							<View style={{ position: "relative", flex: 1 }}>
-								<ShareHeaderComponent />
-								{
-									Share.image_options_open ?
-									<ShareImageOptionsScreen asset={Auth.selected_user?.posting?.post_assets[0]} />
-									:
-									<>
-										<SharePostScreen />
-										{
-											Auth.selected_user?.posting.is_sending_post || Auth.selected_user?.posting.is_adding_bookmark ?
-												<View 
-													style={{ 
-														position: 'absolute',
-														top: 0,
-														height: '100%',
-														width: '100%',
-														zIndex: 10,
-														backgroundColor: App.theme_background_color(),
-														opacity: 0.8
-													}} 
-												>
-													<View style={{
-														height: 200,
-														justifyContent: 'center',
-														alignItems: 'center'              
-													}}>
-														<ActivityIndicator color={App.theme_accent_color()} size={'large'} />
-														<Text style={{ marginTop: 12, color: App.theme_text_color() }}>{ Auth.selected_user?.posting.is_sending_post ? "Sending post..." : "Saving bookmark..." }</Text>
-													</View>
-												</View>
-											: null
-										}
-									</>
-								}
+			<KeyboardProvider statusBarTranslucent={true} navigationBarTranslucent={true}>
+				<View style={{
+					flex: 1,
+					backgroundColor: App.theme_background_color(),
+					marginTop: 52,
+				}}>
+					{
+						Share.is_loading ?
+							<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+								<ActivityIndicator color={App.theme_accent_color()} size="large" />
 							</View>
 							:
-							<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-								<Text style={{ color: App.theme_text_color(), fontSize: 17, textAlign: "center", marginBottom: 10}}>Using the Micro.blog app, please sign in before using the share extension.</Text>
-								<Button title="Open App" onPress={Share.open_in_app} />
-							</View>
-				}
-			</View>
+							Share.is_logged_in() ?
+								<View style={{ position: "relative", flex: 1 }}>
+									<ShareHeaderComponent />
+									{
+										Share.image_options_open ?
+										<ShareImageOptionsScreen asset={Auth.selected_user?.posting?.post_assets[0]} />
+										:
+										<>
+											<SharePostScreen />
+											{
+												Auth.selected_user?.posting.is_sending_post || Auth.selected_user?.posting.is_adding_bookmark ?
+													<View 
+														style={{ 
+															position: 'absolute',
+															top: 0,
+															height: '100%',
+															width: '100%',
+															zIndex: 10,
+															backgroundColor: App.theme_background_color(),
+															opacity: 0.8
+														}} 
+													>
+														<View style={{
+															height: 200,
+															justifyContent: 'center',
+															alignItems: 'center'              
+														}}>
+															<ActivityIndicator color={App.theme_accent_color()} size={'large'} />
+															<Text style={{ marginTop: 12, color: App.theme_text_color() }}>{ Auth.selected_user?.posting.is_sending_post ? "Sending post..." : "Saving bookmark..." }</Text>
+														</View>
+													</View>
+												: null
+											}
+										</>
+									}
+								</View>
+								:
+								<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+									<Text style={{ color: App.theme_text_color(), fontSize: 17, textAlign: "center", marginBottom: 10}}>Using the Micro.blog app, please sign in before using the share extension.</Text>
+									<Button title="Open App" onPress={Share.open_in_app} />
+								</View>
+					}
+				</View>
+			</KeyboardProvider>
 		)
 	}
 
