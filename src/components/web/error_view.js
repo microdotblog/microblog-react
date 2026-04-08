475fe386534e16d9b192f570e5806dfce8b57621
Manton Reece <manton@me.com>
2026-04-08 09:02:54 +0200

    Removed some of the “whoops” messages which are a bit too informal.

--- src/components/web/error_view.js
+++ src/components/web/error_view.js
@@ -9,7 +9,7 @@ export default class WebErrorViewModule extends React.Component{
   render() {
     return (
       <View style={{ flex: 1, height: '100%', position: 'absolute', width: '100%', backgroundColor: App.theme_background_color(), justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ color: App.theme_text_color(), marginTop: 15, fontSize: 17 }}>Whoops, an error occured.</Text>
+        <Text style={{ color: App.theme_text_color(), marginTop: 15, fontSize: 17 }}>Error loading content.</Text>
         <Text style={{ color: App.theme_text_color(), marginTop: 15, fontWeight: '700' }}>{this.props.error_name}</Text>
         <Text style={{ color: App.theme_text_color(), marginTop: 15 }}>Please pull to refresh to try again...</Text>
       </View>
