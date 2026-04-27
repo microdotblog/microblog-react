31144fc80ea3300d6bd6ceefc519d9cf11da45c8
Vincent Ritter <git@vincentritter.com>
2026-04-27 18:08:14 +0200

    Fix native editor theme updates.
    Pass the current app theme into the iOS highlighting text view from React so MobX theme changes reach the native editor.
    
    Reapply native text view colours, keyboard appearance, and redraw state when the explicit colour scheme or iOS trait collection changes.

--- src/components/text/highlighting_text.js
+++ src/components/text/highlighting_text.js
@@ -1,13 +1,17 @@
-import * as React from 'react';
-import { requireNativeComponent, Platform, TextInput } from 'react-native';
+import * as React from 'react'
+import { observer } from 'mobx-react'
+import { requireNativeComponent, Platform, TextInput } from 'react-native'
+import App from '../../stores/App'
 
-const MBHighlightingTextView = requireNativeComponent("MBHighlightingTextView");
+const MBHighlightingTextView = requireNativeComponent('MBHighlightingTextView')
 
+@observer
 export default class HighlightingText extends React.Component {
   render() {
-		if(Platform.OS === "ios"){
-			return( <MBHighlightingTextView {...this.props} /> )
-		}
-		return( <TextInput {...this.props} /> )
+    if (Platform.OS === 'ios') {
+      return <MBHighlightingTextView {...this.props} colorScheme={this.props.colorScheme || App.theme} />
+    }
+
+    return <TextInput {...this.props} />
   }
 }
