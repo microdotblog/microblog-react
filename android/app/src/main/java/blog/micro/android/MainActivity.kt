54a9455a1b8a83a9b74f1f89c6dc96b89fd5e9cb
Vincent Ritter <git@vincentritter.com>
2026-04-27 09:11:55 +0200

    Upgrade React Native to 0.83 and Expo SDK 55.
    - Align React Native, Expo, Metro, Babel, and lockfile dependencies with the 0.83/SDK 55 upgrade.
    - Update iOS Podfile, AppDelegate, privacy metadata, project settings, and resolved pods.
    - Update Android Gradle, application entry points, manifest, and Expo/RN autolinking setup.
    - Add patch-package fixes for Android dependencies that still reference jcenter.
    - Layer React Native and Expo Metro defaults to suppress the RN 0.73+ Metro config warning while retaining Expo behaviour.

--- android/app/src/main/java/blog/micro/android/MainActivity.kt
+++ android/app/src/main/java/blog/micro/android/MainActivity.kt
@@ -1,38 +1,64 @@
-package blog.micro.android;
-import expo.modules.ReactActivityDelegateWrapper
+package blog.micro.android
+
 import android.content.Intent
-import android.os.Bundle;
-import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory;
+import android.os.Build
+import android.os.Bundle
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory
 
+import expo.modules.ReactActivityDelegateWrapper
 
 class MainActivity : ReactActivity() {
 
-    /**
-    * Returns the name of the main component registered from JavaScript. This is used to schedule
-    * rendering of the component.
-    */
-    override fun getMainComponentName(): String = "Micro.blog"
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "Micro.blog"
 
-    /**
-    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-    */
-    override fun createReactActivityDelegate(): ReactActivityDelegate =
-      ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled))
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate {
+    return ReactActivityDelegateWrapper(
+      this,
+      BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
+      object : DefaultReactActivityDelegate(
+        this,
+        mainComponentName,
+        fabricEnabled
+      ) {}
+    )
+  }
 
-    //react-native-screens override
-    override fun onCreate(savedInstanceState: Bundle?) {
-        supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
-        super.onCreate(savedInstanceState);
+  // react-native-screens override
+  override fun onCreate(savedInstanceState: Bundle?) {
+    supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
+    super.onCreate(null)
+  }
+
+  override fun onNewIntent(intent: Intent) {
+    super.onNewIntent(intent)
+    setIntent(intent)
+  }
+
+  /**
+   * Align the back button behavior with Android S
+   * where moving root activities to background instead of finishing activities.
+   */
+  override fun invokeDefaultOnBackPressed() {
+    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+      if (!moveTaskToBack(false)) {
+        super.invokeDefaultOnBackPressed()
+      }
+      return
     }
 
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-        setIntent(intent)
-    }
+    super.invokeDefaultOnBackPressed()
+  }
 }
