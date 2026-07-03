5cef15dbf29cd402811fa8342794526143c59f88
Vincent Ritter <git@vincentritter.com>
2026-07-03 23:11:49 +0200

    Recover iOS webviews after background suspension by pinging on resume and remounting when unresponsive.

--- src/components/web/error_view.js
+++ src/components/web/error_view.js
@@ -1,18 +1,23 @@
-import * as React from 'react';
-import { observer } from 'mobx-react';
-import { ActivityIndicator, View, Text } from "react-native"
+import * as React from 'react'
+import { observer } from 'mobx-react'
+import { View, Text, Pressable } from "react-native"
 import App from '../../stores/App'
 
 @observer
-export default class WebErrorViewModule extends React.Component{
-  
+export default class WebErrorViewModule extends React.Component {
+
   render() {
     return (
-      <View style={{ flex: 1, height: '100%', position: 'absolute', width: '100%', backgroundColor: App.theme_background_color(), justifyContent: 'center', alignItems: 'center' }}>
+      <Pressable
+        onPress={this.props.on_retry}
+        style={{ flex: 1, height: '100%', position: 'absolute', width: '100%', backgroundColor: App.theme_background_color(), justifyContent: 'center', alignItems: 'center' }}
+        accessibilityRole="button"
+        accessibilityLabel="Retry loading content"
+      >
         <Text style={{ color: App.theme_text_color(), marginTop: 15, fontSize: 17 }}>Error loading content.</Text>
         <Text style={{ color: App.theme_text_color(), marginTop: 15, fontWeight: '700' }}>{this.props.error_name}</Text>
-        <Text style={{ color: App.theme_text_color(), marginTop: 15 }}>Please pull to refresh to try again...</Text>
-      </View>
+        <Text style={{ color: App.theme_text_color(), marginTop: 15 }}>Tap to try again...</Text>
+      </Pressable>
     )
   }
 
