03db44987dd53d397b28a559086f6deedc04a1e8
Vincent Ritter <git@vincentritter.com>
2026-08-10 21:03:03 +0200

    Switch token storage fully to expo-secure-store.

--- src/utils/snapshots.js
+++ src/utils/snapshots.js
@@ -1,7 +1,6 @@
 import { onSnapshot } from 'mobx-state-tree';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as SecureStore from 'expo-secure-store';
-import SFInfo from 'react-native-sensitive-info'
 
 import Tokens from './../stores/Tokens';
 import Auth from './../stores/Auth';
@@ -20,8 +19,7 @@ function debounce(func, wait) {
 const debounce_ms = 1500
 
 onSnapshot(Tokens, snapshot => {
-  SFInfo.setItem('Tokens', JSON.stringify(snapshot), {}),
-  SecureStore.setItem('Tokens', JSON.stringify(snapshot), {}),
+  SecureStore.setItem('Tokens', JSON.stringify(snapshot))
   console.log("SNAPSHOT:::TOKENS")
 });
 onSnapshot(Auth, debounce(
